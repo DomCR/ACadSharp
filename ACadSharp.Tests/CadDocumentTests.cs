c717e2687f6efb4d0366bd13437213a788237b46
DomCR <DomCR@users.noreply.github.com>
2022-04-10 17:02:58 +0200

    added test

--- ACadSharp.Tests/CadDocumentTests.cs
+++ ACadSharp.Tests/CadDocumentTests.cs
@@ -6,12 +6,13 @@ using ACadSharp.Tables.Collections;
 using ACadSharp.Tables;
 using Xunit;
 using ACadSharp.Tests.Common;
+using ACadSharp.Entities;
 
 namespace ACadSharp.Tests
 {
 	public class CadDocumentTests
 	{
-		[Fact()]
+		[Fact]
 		public void CadDocumentTest()
 		{
 			CadDocument doc = new CadDocument();
@@ -19,12 +20,38 @@ namespace ACadSharp.Tests
 			DocumentIntegrity.AssertTableHirearchy(doc);
 		}
 
-		[Fact()]
+		[Fact]
 		public void CadDocumentDefaultTest()
 		{
 			CadDocument doc = new CadDocument();
 
 			DocumentIntegrity.AssertDocumentDefaults(doc);
+		}
+
+		[Fact]
+		public void AddCadObject()
+		{
+			Line line = new Line();
+			CadDocument doc = new CadDocument();
+
+			doc.BlockRecords[BlockRecord.ModelSpaceName].Entities.Add(line);
+
+			CadObject l = doc.GetCadObject(line.Handle);
+			Assert.NotNull(l);
+			Assert.Equal(line, l);
+			Assert.False(0 == l.Handle);
+			Assert.Equal(line.Handle, l.Handle);
+		}
+
+		[Fact]
+		public void NotAllowDuplicate()
+		{
+			Line line = new Line();
+			CadDocument doc = new CadDocument();
+
+			doc.BlockRecords[BlockRecord.ModelSpaceName].Entities.Add(line);
+
+			Assert.Throws<ArgumentException>(() => doc.BlockRecords[BlockRecord.ModelSpaceName].Entities.Add(line));
 		}
 	}
 }
