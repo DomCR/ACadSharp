46033c9cdba726f9aced6593004ffc944fc6931f
DomCR <DomCR@users.noreply.github.com>
2022-05-07 23:19:34 +0200

    entities examples

--- ACadSharp.Examples/DocumentExamples.cs
+++ ACadSharp.Examples/DocumentExamples.cs
@@ -8,7 +8,7 @@ using System.Text;
 
 namespace ACadSharp.Examples
 {
-	public static class DocumentExplorationExamples
+	public static class DocumentExamples
 	{
 		/// <summary>
 		/// Get all the entities in the model
@@ -41,6 +41,31 @@ namespace ACadSharp.Examples
 
 			// Get the insert instance that is using the block that you are looking for
 			return modelSpace.Entities.OfType<Insert>().Where(e => e.Block.Name == blockname);
+		}
+
+		/// <summary>
+		/// Example on how to create entities and add them into the drawing
+		/// </summary>
+		public static void CreateEntities()
+		{
+			//Create a new document
+			CadDocument doc = new CadDocument();
+
+			//Create a point located in (10, 10, 0)
+			Point pt = new Point
+			{
+				Location = new CSMath.XYZ(10, 10, 0)
+			};
+
+			//Create a line from the origin to the point (5, 5, 0)
+			Line line = new Line
+			{
+				StartPoint = CSMath.XYZ.Zero,
+				EndPoint = new CSMath.XYZ(5, 5, 0)
+			};
+
+			doc.BlockRecords["*Model_Space"].Entities.Add(pt);
+			doc.BlockRecords["*Model_Space"].Entities.Add(line);
 		}
 	}
 }
--- ACadSharp.Examples/README.md
+++ ACadSharp.Examples/README.md
@@ -1,3 +1,23 @@
 ﻿## ACadSharp Examples
 
 Examples about how to use **ACadSharp** to create and explore dwg and dxf files.
+
+Quick example of how to get all the entities in the drawing:
+
+```c#
+/// <summary>
+/// Get all the entities in the model
+/// </summary>
+/// <param name="file"></param>
+/// <returns></returns>
+public static IEnumerable<Entity> GetAllEntitiesInModel(string file)
+{
+	CadDocument doc = DwgReader.Read(file);
+
+	// Get the model space where all the drawing entities are
+	BlockRecord modelSpace = doc.BlockRecords["*Model_Space"];
+
+	// Get all the entities in the model space
+	return modelSpace.Entities;
+}
+```
