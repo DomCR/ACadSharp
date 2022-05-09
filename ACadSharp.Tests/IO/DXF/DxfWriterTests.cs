4be10fee5b068c1a3c83d8affaf46225bd9f6a60
DomCR <DomCR@users.noreply.github.com>
2022-05-09 17:56:46 +0200

    improved file loading

--- ACadSharp.Tests/IO/DXF/DxfWriterTests.cs
+++ ACadSharp.Tests/IO/DXF/DxfWriterTests.cs
@@ -12,7 +12,7 @@ namespace ACadSharp.Tests.IO.DXF
 {
 	public class DxfWriterTests
 	{
-		private const string _samplesFolder = "../../../../samples/out";
+		private const string _samplesFolder = "../../../../samples";
 
 		protected readonly ITestOutputHelper _output;
 
@@ -33,7 +33,7 @@ namespace ACadSharp.Tests.IO.DXF
 		public void WriteAsciiTest()
 		{
 			CadDocument doc = new CadDocument();
-			string path = Path.Combine(_samplesFolder, "out_empty_sample_ascii.dxf");
+			string path = Path.Combine(_samplesFolder, "out/out_empty_sample_ascii.dxf");
 
 			using (var wr = new DxfWriter(path, doc, false))
 			{
@@ -45,24 +45,24 @@ namespace ACadSharp.Tests.IO.DXF
 				CadDocument readed = re.Read();
 			}
 
-			this.checkDocumentInAutocad(path);
+			this.checkDocumentInAutocad(Path.GetFullPath(path));
 		}
 
 		[Fact(Skip = "Not implemented")]
 		public void WriteBinaryTest()
 		{
 			CadDocument doc = new CadDocument();
-			string path = Path.Combine(_samplesFolder, "out_empty_sample_binary.dxf");
+			string path = Path.Combine(_samplesFolder, "out/out_empty_sample_binary.dxf");
 
 			using (var wr = new DxfWriter(path, doc, true))
 			{
 				wr.Write();
 			}
 
-			using (var re = new DxfReader(path, onNotification))
-			{
-				CadDocument readed = re.Read();
-			}
+			//using (var re = new DxfReader(path, onNotification))
+			//{
+			//	CadDocument readed = re.Read();
+			//}
 
 			this.checkDocumentInAutocad(path);
 		}
@@ -83,19 +83,29 @@ namespace ACadSharp.Tests.IO.DXF
 			{
 				process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
 				process.StartInfo.FileName = "\"D:\\Programs\\Autodesk\\AutoCAD 2023\\accoreconsole.exe\"";
-				process.StartInfo.Arguments = $"/i \"{path}\" /l en - US";
+				process.StartInfo.Arguments = $"/i \"{ Path.Combine(_samplesFolder, "sample_base/empty.dwg")}\" /l en - US";
 				process.StartInfo.UseShellExecute = false;
 				process.StartInfo.RedirectStandardOutput = true;
+				process.StartInfo.RedirectStandardInput = true;
 				process.StartInfo.StandardOutputEncoding = Encoding.ASCII;
 
 				Assert.True(process.Start());
 
+				process.StandardInput.WriteLine($"_DXFIN");
+				process.StandardInput.WriteLine($"{path}");
+
 				string l = process.StandardOutput.ReadLine();
+				bool testPassed = true;
 				while (!process.StandardOutput.EndOfStream)
 				{
 					string li = l.Replace("\0", "");
 					if (!string.IsNullOrEmpty(li))
 					{
+						if (li.Contains("Invalid or incomplete DXF input -- drawing discarded."))
+						{
+							testPassed = false;
+						}
+
 						_output.WriteLine(li);
 					}
 
@@ -111,6 +121,9 @@ namespace ACadSharp.Tests.IO.DXF
 						break;
 					}
 				}
+
+				if (!testPassed)
+					throw new Exception("File loading with accoreconsole failed");
 			}
 			finally
 			{
