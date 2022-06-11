d9711107bf68ea10659cc6387cf6cc3899514d45
DomCR <DomCR@users.noreply.github.com>
2022-06-11 11:06:12 +0200

    added namespace

--- ACadSharp/Entities/InvisibleEdgeFlags.cs
+++ ACadSharp/Entities/InvisibleEdgeFlags.cs
@@ -1,28 +1,32 @@
 ﻿using System;
-/// <summary>
-/// Defines which edges are hidden.
-/// </summary>
-[Flags]
-public enum InvisibleEdgeFlags
+
+namespace ACadSharp.Entities
 {
 	/// <summary>
-	/// No flags equivalent to all edges are visible.
+	/// Defines which edges are hidden.
 	/// </summary>
-	None = 0,
-	/// <summary>
-	/// First edge is invisible.
-	/// </summary>
-	First = 1,
-	/// <summary>
-	/// Second edge is invisible.
-	/// </summary>
-	Second = 2,
-	/// <summary>
-	/// Third edge is invisible.
-	/// </summary>
-	Third = 4,
-	/// <summary>
-	/// Fourth edge is invisible.
-	/// </summary>
-	Fourth = 8
+	[Flags]
+	public enum InvisibleEdgeFlags
+	{
+		/// <summary>
+		/// No flags equivalent to all edges are visible.
+		/// </summary>
+		None = 0,
+		/// <summary>
+		/// First edge is invisible.
+		/// </summary>
+		First = 1,
+		/// <summary>
+		/// Second edge is invisible.
+		/// </summary>
+		Second = 2,
+		/// <summary>
+		/// Third edge is invisible.
+		/// </summary>
+		Third = 4,
+		/// <summary>
+		/// Fourth edge is invisible.
+		/// </summary>
+		Fourth = 8
+	}
 }
