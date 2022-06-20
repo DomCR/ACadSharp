16aa3aeabfa59a8fade32e9c2fbc61cf05fef1d9
DomCR <DomCR@users.noreply.github.com>
2022-06-20 18:31:59 +0200

    dxf read

--- ACadSharp/IO/Templates/CadLwPolylineTemplate.cs
+++ ACadSharp/IO/Templates/CadLwPolylineTemplate.cs
@@ -1,9 +1,55 @@
 ﻿using ACadSharp.Entities;
+using CSMath;
+using System.Linq;
 
 namespace ACadSharp.IO.Templates
 {
 	internal class CadLwPolylineTemplate : CadEntityTemplate
 	{
 		public CadLwPolylineTemplate() : base(new LwPolyline()) { }
+
+		public override bool CheckDxfCode(int dxfcode, object value)
+		{
+			bool found = base.CheckDxfCode(dxfcode, value);
+			if (found)
+				return found;
+
+			LwPolyline lw = this.CadObject as LwPolyline;
+
+			LwPolyline.Vertex last = lw.Vertices.LastOrDefault();
+
+			switch (dxfcode)
+			{
+				case 10:
+					lw.Vertices.Add(new LwPolyline.Vertex(new CSMath.XY((double)value, 0)));
+					return true;
+				case 20:
+					if (last is not null)
+					{
+						last.Location = new CSMath.XY(last.Location.X, (double)value);
+					}
+					return true;
+				case 40:
+					if (last is not null)
+					{
+						last.StartWidth = (double)value;
+					}
+					return true;
+				case 41:
+					if (last is not null)
+					{
+						last.EndWidth = (double)value;
+					}
+					return true;
+				case 42:
+					if (last is not null)
+					{
+						last.Bulge = (double)value;
+					}
+					return true;
+			}
+
+			return found;
+		}
 	}
 }
