dd7cf8c033cba635757a659d4e18f3b91e85c633
DomCR <DomCR@users.noreply.github.com>
2022-06-30 20:17:58 +0200

    object writer

--- ACadSharp/IO/DXF/DxfStreamWriter/DxfSectionWriterBase.cs
+++ ACadSharp/IO/DXF/DxfStreamWriter/DxfSectionWriterBase.cs
@@ -112,49 +112,6 @@ namespace ACadSharp.IO.DXF
 			this.writeMap(map, e);
 		}
 
-		protected void writeObject<T>(T co)
-			where T : CadObject
-		{
-			DxfMap map = null;
-
-			switch (co)
-			{
-				case CadDictionary cadDictionary:
-					this.writeDictionary(cadDictionary);
-					return;
-				case Layout layout:
-					this.writeMappedObject<Layout>(layout);
-					break;
-				default:
-					break;
-			}
-		}
-
-		protected void writeDictionary(CadDictionary e)
-		{
-			this._writer.Write(DxfCode.Start, e.ObjectName);
-
-			this.writeCommonObjectData(e);
-
-			this._writer.Write(DxfCode.Subclass, DxfSubclassMarker.Dictionary);
-
-			this._writer.Write(280, e.HardOwnerFlag);
-			this._writer.Write(281, (int)e.ClonningFlags);
-
-			System.Diagnostics.Debug.Assert(e.EntryNames.Length == e.EntryHandles.Length);
-			for (int i = 0; i < e.EntryNames.Length; i++)
-			{
-				this._writer.Write(3, e.EntryNames[i]);
-				this._writer.Write(350, e.EntryHandles[i]);
-			}
-
-			//Add the entries as objects
-			foreach (CadObject item in e)
-			{
-				this.Holder.Objects.Enqueue(item);
-			}
-		}
-
 		protected abstract void writeSection();
 	}
 }
