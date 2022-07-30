179b7042afca0bec6f77cc0525f5120ceca0ea48
DomCR <DomCR@users.noreply.github.com>
2022-07-30 12:00:10 +0200

    setup

--- ACadSharp/IO/CadReaderConfiguration.cs
+++ ACadSharp/IO/CadReaderConfiguration.cs
@@ -1,15 +1,11 @@
-﻿using System;
-
-namespace ACadSharp.IO.DWG
+﻿namespace ACadSharp.IO
 {
-	[Obsolete]
-	[Flags]
-	public enum DwgReaderFlags
+	public class CadReaderConfiguration
 	{
 		/// <summary>
-		/// None
+		/// Clears the cache after the reading
 		/// </summary>
-		None = 0,
+		public bool ClearChache { get; set; } = true;
 
 		/// <summary>
 		/// Use the Standard Cycling Redundancy Check to verify the integrity of the file.
@@ -18,6 +14,6 @@ namespace ACadSharp.IO.DWG
 		/// DWG file format uses a modification of a standard Cyclic Redundancy Check as an error detecting mechanism, 
 		/// if this flag is enabled the reader will perform this verification to detect any possible error, but it will greatly increase the reading time.
 		/// </remarks>
-		CheckCrc = 1,
+		public bool CrcCheck { get; set; } = false;
 	}
 }
