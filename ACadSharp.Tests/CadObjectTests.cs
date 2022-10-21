6f81870df2c4daa25a2bcab5a2edf24509d52630
DomCR <DomCR@users.noreply.github.com>
2022-10-21 23:12:44 +0200

    data factory

--- ACadSharp.Tests/CadObjectTests.cs
+++ ACadSharp.Tests/CadObjectTests.cs
@@ -2,6 +2,7 @@
 using ACadSharp.Tests.Common;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -9,14 +10,26 @@ namespace ACadSharp.Tests
 {
 	public class CadObjectTests
 	{
-		[Fact]
-		public void CloneTest()
+		public static readonly TheoryData<Type> EntityTypes;
+
+		static CadObjectTests()
 		{
-			Circle c = new Circle();
+			EntityTypes = new TheoryData<Type>();
 
-			Circle copy = c.Clone() as Circle;
+			foreach (var item in DataFactory.GetTypes<Entity>())
+			{
+				EntityTypes.Add(item);
+			}
+		}
 
-			CadObjectTestUtils.AssertEntityClone(c, copy);
+		[Theory]
+		[MemberData(nameof(EntityTypes))]
+		public void Clone(Type entityType)
+		{
+			Entity entity = (Entity)Activator.CreateInstance(entityType);
+			Entity copy = (Entity)Activator.CreateInstance(entityType);
+
+			CadObjectTestUtils.AssertEntityClone(entity, copy);
 		}
 	}
 }
