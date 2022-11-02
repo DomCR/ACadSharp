644bb2da853d5f5ad623a550747faea60b476443
DomCR <DomCR@users.noreply.github.com>
2022-11-02 12:57:40 +0100

    explore doc

--- ACadSharp.Examples/Program.cs
+++ ACadSharp.Examples/Program.cs
@@ -1,20 +1,35 @@
-﻿using ACadSharp.Entities;
+﻿using ACadSharp.Examples.Common;
 using ACadSharp.IO;
-using ACadSharp.IO.DWG;
-using ACadSharp.IO.DXF;
-using ACadSharp.Tables;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
 
 namespace ACadSharp.Examples
 {
 	class Program
 	{
+		const string file = "../../../../samples/sample_AC1021.dwg";
+
 		static void Main(string[] args)
 		{
+			CadDocument doc;
+			using (DwgReader reader = new DwgReader(file, NotificationHelper.LogConsoleNotification))
+			{
+				doc = reader.Read();
+			}
+		}
+
+		/// <summary>
+		/// Logs in the console the document information
+		/// </summary>
+		/// <param name="doc"></param>
+		static void ExploreDocument(CadDocument doc)
+		{
+			Console.WriteLine("SUMMARY INFO:");
+
+
+			foreach (var item in doc.AppIds)
+			{
+
+			}
 		}
 	}
 }
