0c5cd19358c78e8442f4c50e5df0eba6f5483214
DomCR <DomCR@users.noreply.github.com>
2022-12-05 13:23:39 +0100

    test setup

---
+++ ACadSharp.Tests/IO/Issues/Issue_64.cs
@@ -0,0 +1,29 @@
+﻿using ACadSharp.IO;
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+//https://stackoverflow.com/questions/29105838/find-the-center-of-a-circle-x-and-y-position-with-only-2-random-points-and-bul
+//http://iainmnorman.github.io/curvebulgecalc/
+
+namespace ACadSharp.Tests.IO.Issues
+{
+	public class Issue_62 : LocalSampleTests
+	{
+		public Issue_62(ITestOutputHelper output) : base(output)
+		{
+		}
+
+		[Theory]
+		[MemberData(nameof(StressDwgFiles))]
+		public void Issue64Test(string test)
+		{
+			if (string.IsNullOrEmpty(test) || 
+				Path.GetFileNameWithoutExtension(test) != "issue_62")
+				return;
+
+			CadDocument doc = DwgReader.Read(test, this.onNotification);
+
+		}
+	}
+}
