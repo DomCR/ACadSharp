a9e58e263917aca857c4b43fa53d705c0d26b6cd
sraedler <sraedler@no-reply.github>
2023-05-09 09:11:23 +0200

    Added Documentation for ExtendedDataCollection

--- ACadSharp/ExtendedDataCollection.cs
+++ ACadSharp/ExtendedDataCollection.cs
@@ -1,7 +1,6 @@
 ﻿using ACadSharp.Tables;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace ACadSharp
 {
@@ -9,26 +8,38 @@ namespace ACadSharp
 	{
 		private Dictionary<AppId, ExtendedData> _data = new Dictionary<AppId, ExtendedData>();
 
+		/// <summary>Add ExtendedData for a specific AppId to the Dictionary.</summary>
+		/// <param name="app">The AppId object.</param>
+		/// <param name="edata">The ExtendedData object.</param>
 		public void Add(AppId app, ExtendedData edata)
 		{
 			this._data.Add(app, edata);
 		}
 
+		/// <summary>Get ExtendedData for a specific AppId from the Dictionary.</summary>
+		/// <param name="app">The AppId object.</param>
+		/// <param name="edata">The ExtendedData object.</param>
 		public ExtendedData Get(AppId app)
 		{
 			return this._data[app];
 		}
 
+		/// <summary>Try to get ExtendedData for a specific AppId from the Dictionary.</summary>
+		/// <param name="app">The AppId object.</param>
+		/// <param name="value">ExtendedData object.</param>
 		public bool TryGet(AppId app, out ExtendedData value)
 		{
 			return this._data.TryGetValue(app, out value);
 		}
 
+		/// <summary>Check whether a AppId is given in the Dictionary.</summary>
+		/// <param name="app">The AppId object.</param>
 		public bool ContainsKey(AppId app)
 		{
 			return this._data.ContainsKey(app);
 		}
 
+		/// <summary>Clear all Dictionary entries.</summary>
 		public void Clear()
 		{
 			this._data.Clear();
