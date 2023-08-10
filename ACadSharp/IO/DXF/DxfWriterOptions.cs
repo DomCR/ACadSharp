a6ef46ec7768493561981fd7fffc4b1f9182a113
DomCR <DomCR@users.noreply.github.com>
2023-08-10 11:51:56 +0200

    variables

--- ACadSharp/IO/DXF/DxfWriterOptions.cs
+++ ACadSharp/IO/DXF/DxfWriterOptions.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using ACadSharp.Header;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ACadSharp.IO
@@ -7,7 +9,46 @@ namespace ACadSharp.IO
 	{
 		private static readonly string[] _mainVariables = new string[]
 		{
-
+			"$ACADVER",
+			"$DWGCODEPAGE",
+			"$LASTSAVEDBY",
+			"$HANDSEED",
+			"$ANGBASE",
+			"$ANGDIR",
+			"$ATTMODE",
+			"$AUNITS",
+			"$AUPREC",
+			"$CECOLOR",
+			"$CELTSCALE",
+			"$CELTYPE",
+			"$CELWEIGHT",
+			"$CLAYER",
+			"$CMLJUST",
+			"$CMLSCALE",
+			"$CMLSTYLE",
+			"$DIMSTYLE",
+			"$TEXTSIZE",
+			"$TEXTSTYLE",
+			"$LUNITS",
+			"$LUPREC",
+			"$MIRRTEXT",
+			"$EXTNAMES",
+			"$INSBASE",
+			"$INSUNITS",
+			"$LTSCALE",
+			"$LWDISPLAY",
+			"$PDMODE",
+			"$PDSIZE",
+			"$PLINEGEN",
+			"$PSLTSCALE",
+			"$SPLINESEGS",
+			"$SURFU",
+			"$SURFV",
+			"$TDCREATE",
+			"$TDUCREATE",
+			"$TDUPDATE",
+			"$TDUUPDATE",
+			"$TDUPDATE",
 		};
 
 		/// <summary>
@@ -24,7 +65,7 @@ namespace ACadSharp.IO
 		/// <summary>
 		/// Header variables to write in the dxf file
 		/// </summary>
-		public IEnumerable<string> HeaderVariables { get; }
+		public IEnumerable<string> HeaderVariables { get { return this._headerVariables.AsEnumerable(); } }
 
 		private HashSet<string> _headerVariables;
 
@@ -33,19 +74,31 @@ namespace ACadSharp.IO
 			this._headerVariables = new HashSet<string>(_mainVariables);
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="name"></param>
+		/// <exception cref="ArgumentException"></exception>
 		public void AddHeaderVariable(string name)
 		{
-			this._headerVariables.Add(name);
-		}
+			Dictionary<string, CadSystemVariable> map = CadHeader.GetHeaderMap();
 
-		public void RemoveHeaderVariable(string name)
-		{
-			if(_mainVariables.Select(v => v.ToLowerInvariant()).Contains(name.ToLowerInvariant()))
+			if (!map.ContainsKey(name))
 			{
-
+				throw new ArgumentException($"The variable {name} does not exist in the header", nameof(name));
 			}
 
 			this._headerVariables.Add(name);
+		}
+
+		public bool RemoveHeaderVariable(string name)
+		{
+			if (_mainVariables.Select(v => v.ToLowerInvariant()).Contains(name.ToLowerInvariant()))
+			{
+				throw new ArgumentException($"The variable {name} cannot be removed from the set", nameof(name));
+			}
+
+			return this._headerVariables.Remove(name);
 		}
 	}
 }
