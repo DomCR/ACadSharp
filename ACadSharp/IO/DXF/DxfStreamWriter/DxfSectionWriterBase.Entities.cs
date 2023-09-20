778b99ff3461baae4487031762393be52d717cae
DomCR <DomCR@users.noreply.github.com>
2023-09-20 07:51:10 +0200

    unignore

--- ACadSharp/IO/DXF/DxfStreamWriter/DxfSectionWriterBase.Entities.cs
+++ ACadSharp/IO/DXF/DxfStreamWriter/DxfSectionWriterBase.Entities.cs
@@ -16,7 +16,6 @@ namespace ACadSharp.IO.DXF
 				case Hatch:
 				case Mesh:
 				case MLine:
-				case MText:
 				case Solid3D:
 					this.notify($"Entity type not implemented : {entity.GetType().FullName}", NotificationType.NotImplemented);
 					return;
