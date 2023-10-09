d676e0a9c7c1f75e77f56e467315a7171ffcbba9
DomCR <DomCR@users.noreply.github.com>
2023-10-09 13:00:00 +0200

    added tests

--- ACadSharp.Tests/IO/IOTests.cs
+++ ACadSharp.Tests/IO/IOTests.cs
@@ -48,7 +48,28 @@ namespace ACadSharp.Tests.IO
 
 		[Theory]
 		[MemberData(nameof(DwgFilePaths))]
-		public void DwgEntitiesToNewFile(string test)
+		public void DwgEntitiesToDwgFile(string test)
+		{
+			CadDocument doc = DwgReader.Read(test);
+
+			CadDocument transfer = new CadDocument();
+			transfer.Header.Version = doc.Header.Version;
+
+			List<Entity> entities = new List<Entity>(doc.Entities);
+			foreach (var item in entities)
+			{
+				Entity e = doc.Entities.Remove(item);
+				transfer.Entities.Add(e);
+			}
+
+			string file = Path.GetFileNameWithoutExtension(test);
+			string pathOut = Path.Combine(_samplesOutFolder, $"{file}_moved_out.dwg");
+			this.writeDwgFile(pathOut, transfer, false);
+		}
+
+		[Theory]
+		[MemberData(nameof(DwgFilePaths))]
+		public void DwgEntitiesToDxfFile(string test)
 		{
 			CadDocument doc = DwgReader.Read(test);
 
@@ -65,6 +86,33 @@ namespace ACadSharp.Tests.IO
 			string file = Path.GetFileNameWithoutExtension(test);
 			string pathOut = Path.Combine(_samplesOutFolder, $"{file}_moved_out.dxf");
 			this.writeDxfFile(pathOut, transfer, true);
+		}
+
+		private void writeCadFile(string file, CadWriterBase writer, bool check)
+		{
+			using (writer)
+			{
+				writer.OnNotification += this.onNotification;
+				writer.Write();
+			}
+
+			if (check)
+				this.checkDxfDocumentInAutocad(Path.GetFullPath(file));
+		}
+
+		private void writeDwgFile(string file, CadDocument doc, bool check)
+		{
+			if (doc.Header.Version < ACadVersion.AC1014 || doc.Header.Version > ACadVersion.AC1018)
+				return;
+
+			using (DwgWriter writer = new DwgWriter(file, doc))
+			{
+				writer.OnNotification += this.onNotification;
+				writer.Write();
+			}
+
+			if (true)
+				this.checkDwgDocumentInAutocad(Path.GetFullPath(file));
 		}
 
 		private void writeDxfFile(string file, CadDocument doc, bool check)
