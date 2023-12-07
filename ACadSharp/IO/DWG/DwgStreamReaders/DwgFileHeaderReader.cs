b1a1075dd36343bd8695182508b9a9eba575cd57
DomCR <DomCR@users.noreply.github.com>
2023-12-07 09:16:25 +0100

    dev proj

--- ACadSharp/IO/DWG/DwgStreamReaders/DwgFileHeaderReader.cs
+++ ACadSharp/IO/DWG/DwgStreamReaders/DwgFileHeaderReader.cs
@@ -1,4 +1,5 @@
-﻿using CSUtilities.IO;
+﻿using ACadSharp.Exceptions;
+using CSUtilities.IO;
 using System;
 using System.IO;
 using System.Threading;
@@ -20,6 +21,53 @@ namespace ACadSharp.IO.DWG
 		public async Task<DwgFileHeader> ReadAsync(CancellationToken cancellationToken = default)
 		{
 			throw new NotImplementedException();
+		}
+
+		public DwgFileHeader Read()
+		{
+			DwgFileHeader fileHeader = DwgFileHeader.CreateFileHeader(this._version);
+
+			//Reset the stream position at the begining
+			this._stream.Position = 0L;
+
+			//Read the file header
+			switch (fileHeader.AcadVersion)
+			{
+				case ACadVersion.Unknown:
+					throw new DwgNotSupportedException();
+				case ACadVersion.MC0_0:
+				case ACadVersion.AC1_2:
+				case ACadVersion.AC1_4:
+				case ACadVersion.AC1_50:
+				case ACadVersion.AC2_10:
+				case ACadVersion.AC1002:
+				case ACadVersion.AC1003:
+				case ACadVersion.AC1004:
+				case ACadVersion.AC1006:
+				case ACadVersion.AC1009:
+					throw new DwgNotSupportedException(this._version);
+				case ACadVersion.AC1012:
+				case ACadVersion.AC1014:
+				case ACadVersion.AC1015:
+					this.readFileHeaderAC15(fileHeader as DwgFileHeaderAC15, _stream);
+					break;
+				case ACadVersion.AC1018:
+					this.readFileHeaderAC18(fileHeader as DwgFileHeaderAC18, _stream);
+					break;
+				case ACadVersion.AC1021:
+					this.readFileHeaderAC21(fileHeader as DwgFileHeaderAC21, _stream);
+					break;
+				case ACadVersion.AC1024:
+				case ACadVersion.AC1027:
+				case ACadVersion.AC1032:
+					//Check if it works...
+					this.readFileHeaderAC18(fileHeader as DwgFileHeaderAC18, _stream);
+					break;
+				default:
+					break;
+			}
+
+			return fileHeader;
 		}
 	}
 }
