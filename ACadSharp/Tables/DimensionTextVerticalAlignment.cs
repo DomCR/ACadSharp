985f10ed4bb542a08836667250ebb60a60192c33
Dr. Matthias Menningen <matthias.menningen@nanologika.de>
2023-12-19 13:44:49 +0100

    -  Documentation: crefs added.

--- ACadSharp/Tables/DimensionTextVerticalAlignment.cs
+++ ACadSharp/Tables/DimensionTextVerticalAlignment.cs
@@ -13,18 +13,21 @@
 		/// <summary>
 		/// Places the dimension text above the dimension line except when the dimension
 		/// line is not horizontal and text inside the extension lines is forced horizontal
-		/// (DIMTIH = 1). The distance from the dimension line to the baseline of the lowest
-		/// line of text is the current DIMGAP value.
+		/// (<see cref="DimensionStyle.TextInsideHorizontal"/> = true). The distance from
+		/// the dimension line to the baseline of the lowest line of text is the current
+		/// <see cref="DimensionStyle.DimensionLineGap" /> value.
 		/// </summary>
 		Above = 1,
 
 		/// <summary>
-		/// Places the dimension text on the side of the dimension line farthest away from the first defining point.
+		/// Places the dimension text on the side of the dimension line farthest away from
+		/// the first defining point.
 		/// </summary>
 		Outside = 2,
 
 		/// <summary>
-		/// Places the dimension text to conform to a Japanese Industrial Standards (JIS) representation.
+		/// Places the dimension text to conform to a Japanese Industrial Standards (JIS)
+		/// representation.
 		/// </summary>
 		JIS = 3,
 
