3e86293f0b953f187341d4ff7046b185f3c6292a
DomCR <DomCR@users.noreply.github.com>
2024-03-06 17:03:03 +0100

    read angle fix

--- ACadSharp/IO/DXF/DxfStreamReader/DxfSectionReaderBase.cs
+++ ACadSharp/IO/DXF/DxfStreamReader/DxfSectionReaderBase.cs
@@ -343,7 +343,7 @@ namespace ACadSharp.IO.DXF
 				case 50:
 					var dim = new DimensionLinear();
 					tmp.SetDimensionObject(dim);
-					dim.Rotation = this._reader.ValueAsDouble;
+					dim.Rotation = CSMath.MathUtils.DegToRad(this._reader.ValueAsDouble);
 					map.SubClasses.Add(DxfSubclassMarker.LinearDimension, DxfClassMap.Create<DimensionLinear>());
 					return true;
 				case 70:
