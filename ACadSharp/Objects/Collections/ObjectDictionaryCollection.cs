24e4aff1b346ca659487aef91d0cbacc0a9a3945
DomCR <DomCR@users.noreply.github.com>
2024-03-15 12:55:52 +0100

    ObjectDictionaryCollection docs

--- ACadSharp/Objects/Collections/ObjectDictionaryCollection.cs
+++ ACadSharp/Objects/Collections/ObjectDictionaryCollection.cs
@@ -4,6 +4,10 @@ using System.Linq;
 
 namespace ACadSharp.Objects.Collections
 {
+	/// <summary>
+	/// Object collection linked to a dictionary
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
 	public abstract class ObjectDictionaryCollection<T> : IEnumerable<T>
 		where T : CadObject, IDictionaryEntry
 	{
@@ -16,21 +20,32 @@ namespace ACadSharp.Objects.Collections
 			this._dictionary = dictionary;
 		}
 
+		/// <summary>
+		/// Add an entry to the collection
+		/// </summary>
+		/// <param name="entry"></param>
 		public void Add(T entry)
 		{
 			this._dictionary.Add(entry.Name, entry);
 		}
 
+		/// <summary>
+		/// Remove an entry from the collection
+		/// </summary>
+		/// <param name="entry"></param>
+		/// <returns></returns>
 		public T Remove(T entry)
 		{
 			return (T)this._dictionary.Remove(entry.Name);
 		}
 
+		/// <inheritdoc/>
 		public IEnumerator<T> GetEnumerator()
 		{
 			return this._dictionary.OfType<T>().GetEnumerator();
 		}
 
+		/// <inheritdoc/>
 		IEnumerator IEnumerable.GetEnumerator()
 		{
 			return this._dictionary.OfType<T>().GetEnumerator();
