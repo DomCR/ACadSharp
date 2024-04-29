6e567230ae678bdde7dd8c3c0d63f7a53f83266c
DomCR <DomCR@users.noreply.github.com>
2024-04-29 09:29:04 +0200

    check for changes

--- update-wiki.sh
+++ update-wiki.sh
@@ -48,6 +48,10 @@ message=`git log -1 --format="%s"`
 git config --local user.email "$email"
 git config --local user.name "$author" 
 git add .
+if git diff-index --quiet HEAD; then
+  echo "Nothing changed"
+  exit 0
+fi
 
 echo "Pushing changes to wiki"
 git commit -m "$message" && git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.wiki.git"
