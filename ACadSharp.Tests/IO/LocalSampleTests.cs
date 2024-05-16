3334b7d431f1e1c558727943dd142db4da8d0094
DomCR <DomCR@users.noreply.github.com>
2024-05-16 12:48:40 +0200

    update

--- ACadSharp.Tests/IO/LocalSampleTests.cs
+++ ACadSharp.Tests/IO/LocalSampleTests.cs
@@ -9,18 +9,12 @@ namespace ACadSharp.Tests.IO
 {
 	public class LocalSampleTests : IOTestsBase
 	{
-		public static TheoryData<string> StressDwgFiles { get; } = new TheoryData<string>();
-
-		public static TheoryData<string> StressDxfFiles { get; } = new TheoryData<string>();
-
 		public static TheoryData<string> UserDwgFiles { get; } = new TheoryData<string>();
 
 		public static TheoryData<string> UserDxfFiles { get; } = new TheoryData<string>();
 
 		static LocalSampleTests()
 		{
-			loadSamples("stress", "dwg", StressDwgFiles);
-			loadSamples("stress", "dxf", StressDxfFiles);
 			loadSamples("user_files", "dwg", UserDwgFiles);
 			loadSamples("user_files", "dxf", UserDxfFiles);
 		}
@@ -37,6 +31,8 @@ namespace ACadSharp.Tests.IO
 				return;
 
 			CadDocument doc = DwgReader.Read(test, this._dwgConfiguration, this.onNotification);
+
+			DwgWriter.Write(new MemoryStream(), doc, this.onNotification);
 
 			return;
 
