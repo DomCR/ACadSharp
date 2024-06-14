02050a7cb0d8e55ca4d83c5234427e02ad6c2909
DomCR <DomCR@users.noreply.github.com>
2024-06-14 14:43:52 +0200

    fix

--- ACadSharp.Tests/Internal/DwgObjectWriterTests.cs
+++ ACadSharp.Tests/Internal/DwgObjectWriterTests.cs
@@ -131,7 +131,7 @@ namespace ACadSharp.Tests.Internal
 			docResult.Header = new ACadSharp.Header.CadHeader();
 			docResult.Header.Version = docToWrite.Header.Version;
 
-			DwgDocumentBuilder builder = new DwgDocumentBuilder(docResult, new DwgReaderConfiguration());
+			DwgDocumentBuilder builder = new DwgDocumentBuilder(docToWrite.Header.Version, docResult, new DwgReaderConfiguration());
 			builder.HeaderHandles.DICTIONARY_LAYOUTS = docToWrite.RootDictionary[CadDictionary.AcadLayout].Handle;
 
 			IDwgStreamReader sreader = DwgStreamReaderBase.GetStreamHandler(docToWrite.Header.Version, stream, resetPositon: true);
