7175a3ffa3cf6e4bcca91f69fe00277625c053a5
DomCR <DomCR@users.noreply.github.com>
2024-10-04 12:13:49 +0200

    pattern clone fix

--- src/ACadSharp.Tests/IO/LocalSampleTests.cs
+++ src/ACadSharp.Tests/IO/LocalSampleTests.cs
@@ -37,22 +37,10 @@ namespace ACadSharp.Tests.IO
 
 			CadDocument newDoc = new CadDocument();
 
-			var h = doc.GetCadObject(7819);
-
 			foreach (var entity in doc.Entities)
 			{
 				doc.Entities.Remove(entity);
 				newDoc.Entities.Add(entity);
-			}
-
-		//	var h = doc.GetCadObject(7819);
-
-			var b1 = doc.BlockRecords["3D ORBIT"];
-			var b2 = doc.BlockRecords["3D ORBIT"];
-
-			using (DwgWriter wr = new DwgWriter(Path.Combine(TestVariables.DesktopFolder, "test.dwg"), newDoc))
-			{
-				wr.Write();
 			}
 		}
 
