edaa0a33748bb82136fac9ed731ce4117b1bb290
DomCR <DomCR@users.noreply.github.com>
2024-12-02 17:28:37 +0100

    cols n rows

---
+++ src/ACadSharp/Entities/TableEntity.Row.cs
@@ -0,0 +1,21 @@
+﻿using ACadSharp.Attributes;
+using System.Collections.Generic;
+
+namespace ACadSharp.Entities
+{
+	public partial class TableEntity
+	{
+		public class Row
+		{
+			/// <summary>
+			/// Row height.
+			/// </summary>
+			[DxfCodeValue(141)]
+			public double Height { get; set; }
+
+			public CellStyle StyleOverride { get; set; } = new();
+
+			public List<Cell> Cells { get; set; } = new();
+		}
+	}
+}
