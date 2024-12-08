65b9f02a56beb2a9030d0ca4caad971b5debe322
mdmozibur <thisismuzib@gmail.com>
2024-12-08 13:13:11 +0600

    removed tests folder

--- src/ACadSharp.Tests/TestModels/BlockRecordNode.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace ACadSharp.Tests.TestModels
-{
-	public class BlockRecordNode : TableEntryNode
-	{
-		public List<EntityNode> Entities { get; set; } = new List<EntityNode>();
-	}
-}
