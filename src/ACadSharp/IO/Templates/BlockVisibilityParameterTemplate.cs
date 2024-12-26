e4b16956e121cf42360630b9ca8893aac60c0dad
DomCR <DomCR@users.noreply.github.com>
2024-12-26 09:31:22 +0100

    fix

--- src/ACadSharp/IO/Templates/BlockVisibilityParameterTemplate.cs
+++ src/ACadSharp/IO/Templates/BlockVisibilityParameterTemplate.cs
@@ -3,36 +3,42 @@
 using ACadSharp.Entities;
 using ACadSharp.Objects;
 
-namespace ACadSharp.IO.Templates {
+namespace ACadSharp.IO.Templates
+{
 
-	internal class BlockVisibilityParameterTemplate : CadTemplate<BlockVisibilityParameter> {
+	internal class BlockVisibilityParameterTemplate : CadTemplate<BlockVisibilityParameter>
+	{
 
 		public BlockVisibilityParameterTemplate(BlockVisibilityParameter cadObject)
-			: base(cadObject) {
+			: base(cadObject)
+		{
 		}
 
-		public IDictionary<ulong, Entity> TotalEntityHandles { get; } = new Dictionary<ulong, Entity>();
+		public List<ulong> EntityHandles { get; } = new ();
 
 		public IDictionary<BlockVisibilityParameter.SubBlock, IList<ulong>> SubBlockHandles { get; } = new Dictionary<BlockVisibilityParameter.SubBlock, IList<ulong>>();
 
-		public override void Build(CadDocumentBuilder builder) {
+		public override void Build(CadDocumentBuilder builder)
+		{
 			base.Build(builder);
 
-			foreach (var cadObjectHandle in this.TotalEntityHandles) {
-				ulong handle = cadObjectHandle.Key;
-				if (builder.TryGetCadObject(handle, out Entity entity)) {
-					this.TotalEntityHandles[handle] = entity;
+			foreach (var handle in this.EntityHandles)
+			{
+				if (builder.TryGetCadObject(handle, out Entity entity))
+				{
 					this.CadObject.Entities.Add(entity);
 				}
 			}
 
-			foreach (var subGroup in this.CadObject.SubBlocks) {
-				if (this.SubBlockHandles.TryGetValue(subGroup, out IList<ulong> subBlockHandles)) {
-					foreach (ulong handle in subBlockHandles) {
-						if (this.TotalEntityHandles.TryGetValue(handle, out Entity entity)) {
+			foreach (var subGroup in this.CadObject.SubBlocks)
+			{
+				if (this.SubBlockHandles.TryGetValue(subGroup, out IList<ulong> subBlockHandles))
+				{
+					foreach (ulong handle in subBlockHandles)
+					{
+						if (builder.TryGetCadObject(handle, out Entity entity))
+						{
 							subGroup.Entities.Add(entity);
-						}
-						else if (builder.TryGetCadObject(handle, out Entity entityX)) {
 						}
 					}
 				}
