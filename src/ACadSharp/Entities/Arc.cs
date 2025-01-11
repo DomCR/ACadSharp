4685ce1d66d799e9031e51d43819274e6f175827
DomCR <DomCR@users.noreply.github.com>
2025-01-11 19:22:39 +0100

    ConvertToArc

--- src/ACadSharp/Entities/Arc.cs
+++ src/ACadSharp/Entities/Arc.cs
@@ -119,8 +119,8 @@ namespace ACadSharp.Entities
 				double cosine = this.Radius * Math.Cos(angle);
 				double sine = this.Radius * Math.Sin(angle);
 
-				cosine = MathUtils.IsZero(cosine) ? 0 : cosine;
-				sine = MathUtils.IsZero(sine) ? 0 : sine;
+				cosine = MathHelper.IsZero(cosine) ? 0 : cosine;
+				sine = MathHelper.IsZero(sine) ? 0 : sine;
 
 				ocsVertexes.Add(new XY(cosine + this.Center.X, sine + this.Center.Y));
 			}
--- src/ACadSharp/Entities/Hatch.BoundaryPath.Arc.cs
+++ src/ACadSharp/Entities/Hatch.BoundaryPath.Arc.cs
@@ -1,5 +1,8 @@
 ﻿using ACadSharp.Attributes;
 using CSMath;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ACadSharp.Entities
 {
@@ -48,7 +51,78 @@ namespace ACadSharp.Entities
 				/// <inheritdoc/>
 				public override BoundingBox GetBoundingBox()
 				{
-					throw new System.NotImplementedException();
+					List<XY> vertices = this.PolygonalVertexes(256);
+					return BoundingBox.FromPoints(vertices.Select(v => (XYZ)v));
+				}
+
+				/// <summary>
+				/// Converts the arc in a list of vertexes.
+				/// </summary>
+				/// <param name="precision">Number of vertexes generated.</param>
+				/// <returns>A list vertexes that represents the arc expressed in object coordinate system.</returns>
+				public List<XY> PolygonalVertexes(int precision)
+				{
+					if (precision < 2)
+					{
+						throw new ArgumentOutOfRangeException(nameof(precision), precision, "The arc precision must be equal or greater than two.");
+					}
+
+					List<XY> ocsVertexes = new List<XY>();
+
+					double start;
+					double end;
+					if (this.CounterClockWise)
+					{
+						start = this.StartAngle;
+						end = this.EndAngle;
+					}
+					else
+					{
+						start = 2 * Math.PI - this.EndAngle;
+						end = 2 * Math.PI - this.StartAngle;
+					}
+
+					if (end < start)
+					{
+						end += 2 * Math.PI;
+					}
+
+					double delta = (end - start) / (precision - 1);
+					for (int i = 0; i < precision; i++)
+					{
+						double angle = start + delta * i;
+						double cosine = this.Radius * Math.Cos(angle);
+						double sine = this.Radius * Math.Sin(angle);
+
+						cosine = MathHelper.IsZero(cosine) ? 0 : cosine;
+						sine = MathHelper.IsZero(sine) ? 0 : sine;
+
+						ocsVertexes.Add(new XY(cosine + this.Center.X, sine + this.Center.Y));
+					}
+
+					return ocsVertexes;
+				}
+
+				public ACadSharp.Entities.Arc ConvertToArc()
+				{
+					if (this.CounterClockWise)
+					{
+						return new ACadSharp.Entities.Arc
+						{
+							Center = (XYZ)this.Center,
+							Radius = this.Radius,
+							StartAngle = this.StartAngle,
+							EndAngle = this.EndAngle
+						};
+					}
+
+					return new ACadSharp.Entities.Arc
+					{
+						Center = (XYZ)this.Center,
+						Radius = this.Radius,
+						StartAngle = 2 * Math.PI - this.EndAngle,
+						EndAngle = 2 * Math.PI - this.StartAngle
+					};
 				}
 			}
 		}
