4a0512d4e28b3d0e6d5c4779dbe285940abc02ae
DomCR <DomCR@users.noreply.github.com>
2025-01-22 10:44:09 +0100

    docs

--- src/ACadSharp/XData/ExtendedData.cs
+++ src/ACadSharp/XData/ExtendedData.cs
@@ -1,5 +1,4 @@
 ﻿using ACadSharp.Tables;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
--- src/ACadSharp/XData/ExtendedDataReference.cs
+++ src/ACadSharp/XData/ExtendedDataReference.cs
@@ -1,17 +1,21 @@
-﻿using System;
-
-namespace ACadSharp.XData
+﻿namespace ACadSharp.XData
 {
 	public abstract class ExtendedDataReference<T> : ExtendedDataRecord<ulong>, IExtendedDataHandleReference
 		where T : CadObject
 	{
 		protected ExtendedDataReference(DxfCode code, ulong handle) : base(code, handle) { }
 
+		/// <summary>
+		/// Resolve the reference of for an specific <see cref="CadDocument"/>.
+		/// </summary>
+		/// <param name="document"></param>
+		/// <returns></returns>
 		public T ResolveReference(CadDocument document)
 		{
 			return document.GetCadObject<T>(this.Value);
 		}
 
+		/// <inheritdoc/>
 		CadObject IExtendedDataHandleReference.ResolveReference(CadDocument document)
 		{
 			return this.ResolveReference(document);
