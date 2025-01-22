7d951719a68e08f1c674901485b602dda1f856d1
DomCR <DomCR@users.noreply.github.com>
2025-01-22 14:52:19 +0100

    test refine

--- src/ACadSharp.Tests/IO/LoadPatternsTests.cs
+++ src/ACadSharp.Tests/IO/LoadPatternsTests.cs
@@ -1,6 +1,8 @@
 ﻿using ACadSharp.Entities;
 using ACadSharp.IO;
+using ACadSharp.Tests.Common;
 using ACadSharp.Tests.TestModels;
+using CSMath;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -45,10 +47,23 @@ namespace ACadSharp.Tests.IO
 
 			foreach (HatchPattern pattern in patterns)
 			{
+				var h = hatches.FirstOrDefault(h => h.Pattern.Name.Equals(pattern.Name, System.StringComparison.OrdinalIgnoreCase));
 				var p = hatches.FirstOrDefault(h => h.Pattern.Name.Equals(pattern.Name, System.StringComparison.OrdinalIgnoreCase)).Pattern;
 
 				Assert.NotNull(p);
 				Assert.Equal(pattern.Lines.Count, p.Lines.Count);
+
+				for (int i = 0; i < pattern.Lines.Count; i++)
+				{
+					HatchPattern.Line l1 = p.Lines[i];
+					HatchPattern.Line l2 = pattern.Lines[i];
+
+					Assert.Equal(MathHelper.Cos(l1.Angle), MathHelper.Cos(l2.Angle), 0.001);
+					Assert.Equal(MathHelper.Sin(l1.Angle), MathHelper.Sin(l2.Angle), 0.001);
+					AssertUtils.AreEqual(l1.BasePoint.Round(5) / h.PatternScale, l2.BasePoint.Round(5));
+					Assert.Equal(l1.DashLengths.Count, l2.DashLengths.Count);
+					Assert.Equal(l1.Offset.Round(5) / h.PatternScale, l2.Offset.Round(5));
+				}
 			}
 		}
 	}
