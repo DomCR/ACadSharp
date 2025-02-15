b58cccbcde979f9123f5a281e03b5abd13529a68
DomCR <DomCR@users.noreply.github.com>
2025-02-15 16:09:35 +0100

    line translation

--- src/ACadSharp.Tests/Entities/LineTests.cs
+++ src/ACadSharp.Tests/Entities/LineTests.cs
@@ -1,4 +1,5 @@
 ﻿using ACadSharp.Entities;
+using ACadSharp.Tests.Common;
 using CSMath;
 using Xunit;
 
@@ -6,6 +7,47 @@ namespace ACadSharp.Tests.Entities
 {
 	public class LineTests
 	{
+		private CSMathRandom _random = new CSMathRandom();
+
+		[Fact]
+		public void TranslationTest()
+		{
+			var start = XYZ.Zero;
+			var end = new XYZ(1, 1, 0);
+			Line line = new Line
+			{
+				StartPoint = start,
+				EndPoint = end,
+			};
+
+			XYZ move = new XYZ(5, 5, 0);
+			Transform translation = Transform.CreateTranslation(move);
+			line.ApplyTransform(translation);
+
+			AssertUtils.AreEqual(start.Add(move), line.StartPoint);
+			AssertUtils.AreEqual(end.Add(move), line.EndPoint);
+		}
+
+		[Fact]
+		public void RandomTranslationTest()
+		{
+			XYZ start = this._random.Next<XYZ>();
+			XYZ end = this._random.Next<XYZ>();
+			Line line = new Line
+			{
+				StartPoint = start,
+				EndPoint = end,
+			};
+
+			XYZ move = this._random.Next<XYZ>();
+			Transform translation = Transform.CreateTranslation(move);
+			line.ApplyTransform(translation);
+
+			AssertUtils.AreEqual(start.Add(move), line.StartPoint);
+			AssertUtils.AreEqual(end.Add(move), line.EndPoint);
+		}
+
+
 		[Fact]
 		public void GetBoundingBoxTest()
 		{
