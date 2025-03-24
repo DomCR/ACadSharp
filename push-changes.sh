106d08cfee0b2dc79fc7cbd2ea651c6217376086
DomCR <DomCR@users.noreply.github.com>
2025-03-24 12:28:05 +0100

    commit

--- push-changes.sh
+++ push-changes.sh
@@ -5,12 +5,12 @@ email=`git log -1 --format="%ae"`
 
 git config --local user.email "$email"
 git config --local user.name "$author" 
-git add .
+git add -A
 if git diff-index --quiet HEAD; then
   echo "Nothing changed"
   exit 0
 fi
 
-echo "Pushing changes"
+echo "Pushing changes $BRANCH"
 git commit -m "Update project version to $VERSION"
 git push origin $BRANCH
