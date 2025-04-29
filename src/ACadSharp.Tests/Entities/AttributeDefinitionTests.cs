376764a32bf03f5da667af96d3c8d32cc9da6190
DomCR <DomCR@users.noreply.github.com>
2025-04-29 10:24:12 +0200

    coverage

---
+++ src/ACadSharp.Tests/Entities/AttributeDefinitionTests.cs
@@ -0,0 +1,19 @@
+﻿using ACadSharp.Entities;
+using ACadSharp.Tests.Common;
+using Xunit;
+
+namespace ACadSharp.Tests.Entities
+{
+	public class AttributeDefinitionTests : CommonEntityTests<AttributeDefinition>
+	{
+		[Fact]
+		public void AttConstructor()
+		{
+			AttributeEntity att = EntityFactory.CreateDefault<AttributeEntity>();
+
+			AttributeDefinition attributeDefinition = new AttributeDefinition(att);
+
+			EntityComparator.IsEqual(att, attributeDefinition);
+		}
+	}
+}
