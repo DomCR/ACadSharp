04450b90b140e7e6596876563ffdf96145931468
DomCR <DomCR@users.noreply.github.com>
2025-05-03 11:58:53 +0200

    test cases

---
+++ src/ACadSharp.Tests/Entities/DimensionDiameterTests.cs
@@ -0,0 +1,9 @@
+﻿using ACadSharp.Entities;
+
+namespace ACadSharp.Tests.Entities
+{
+	public class DimensionDiameterTests : CommonDimensionTests<DimensionDiameter>
+	{
+		public override DimensionType Type => DimensionType.Diameter;
+	}
+}
