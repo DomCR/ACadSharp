7144cfccfa524c76ef4a85094740350f006bda73
DomCR <domenech93@gmail.com>
2025-06-16 10:29:11 +0200

    test added

---
+++ src/ACadSharp.Tests/Tables/LineTypeTests.cs
@@ -0,0 +1,42 @@
+﻿using ACadSharp.Extensions;
+using ACadSharp.Tables;
+using ACadSharp.Tests.Common;
+using System.Linq;
+using Xunit;
+
+namespace ACadSharp.Tests.Tables
+{
+	public class LineTypeTests
+	{
+		[Fact]
+		public void CloneTest()
+		{
+			LineType lt = new LineType("segmented");
+			lt.Description = "line type description";
+
+			LineType.Segment s1 = new LineType.Segment
+			{
+				Length = 12,
+				//Style = this.Document.TextStyles[TextStyle.DefaultName]
+			};
+
+			LineType.Segment s2 = new LineType.Segment
+			{
+				Length = -3,
+				//Style = this.Document.TextStyles[TextStyle.DefaultName]
+			};
+
+			lt.AddSegment(s1);
+			lt.AddSegment(s2);
+
+			LineType clone = lt.CloneTyped();
+
+			CadObjectTestUtils.AssertTableEntryClone(lt, clone);
+
+			for (int i = 0; i < lt.Segments.Count(); i++)
+			{
+				Assert.Equal(lt.Segments.ElementAt(i).Length, clone.Segments.ElementAt(i).Length);
+			}
+		}
+	}
+}
