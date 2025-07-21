189dbb3270e0963899f446e6e440a0bcdbc82c19
DomCR <domenech93@gmail.com>
2025-07-21 17:47:03 +0200

    writer options

--- src/ACadSharp/IO/CadWriterConfiguration.cs
+++ src/ACadSharp/IO/CadWriterConfiguration.cs
@@ -16,15 +16,37 @@ namespace ACadSharp.IO
 		public bool CloseStream { get; set; } = true;
 
 		/// <summary>
-		/// The writer will not ignore the <see cref="ACadSharp.Objects.XRecord"/> objects in the document.
+		/// Resets the <see cref="DxfClass"/> collection in the <see cref="CadDocument"/> before writing it.
 		/// </summary>
 		/// <remarks>
-		/// Due the complexity of XRecords, if this flag is set to true, it may cause a corruption of the file.
+		/// Sometimes the files are corrupted by badly formed dxf classes, is recommended to keep this flag set.
+		/// </remarks>
+		/// <value>
+		/// default: true
+		/// </value>
+		public bool ResetDxfClasses { get; set; } = true;
+
+		/// <summary>
+		///  Update the blocks that visualize the dimensions in the model space.
+		/// </summary>
+		/// <remarks>
+		/// When creating or modifying a dimension in a block, it needs to be updated in order to appear in the drawing.
 		/// </remarks>
 		/// <value>
 		/// default: false
 		/// </value>
-		public bool WriteXRecords { get; set; } = false;
+		public bool UpdateDimensionsInBlocks { get; set; } = false;
+
+		/// <summary>
+		/// Update the blocks that visualize the dimensions in the blocks.
+		/// </summary>
+		/// <remarks>
+		/// The dimensions in the model space are automatically updated by the drawing software, is not recommended to update them if is not needed.
+		/// </remarks>
+		/// <value>
+		/// default: false
+		/// </value>
+		public bool UpdateDimensionsInModel { get; set; } = false;
 
 		/// <summary>
 		/// The writer will not ignore the <see cref="ACadSharp.XData.ExtendedData"/> collection in the <see cref="CadObject"/>.
@@ -35,14 +57,14 @@ namespace ACadSharp.IO
 		public bool WriteXData { get; set; } = true;
 
 		/// <summary>
-		/// Resets the <see cref="DxfClass"/> collection in the <see cref="CadDocument"/> before writing it.
+		/// The writer will not ignore the <see cref="ACadSharp.Objects.XRecord"/> objects in the document.
 		/// </summary>
 		/// <remarks>
-		/// Sometimes the files are corrupted by badly formed dxf classes, is recommended to keep this flag set.
+		/// Due the complexity of XRecords, if this flag is set to true, it may cause a corruption of the file.
 		/// </remarks>
 		/// <value>
-		/// default: true
+		/// default: false
 		/// </value>
-		public bool ResetDxfClasses { get; set; } = true;
+		public bool WriteXRecords { get; set; } = false;
 	}
 }
