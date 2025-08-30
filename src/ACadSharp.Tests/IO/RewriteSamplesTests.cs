dd155259c7ed0e0322176294b45d1aa3d5136c3a
DomCR <domenech93@gmail.com>
2025-08-30 16:10:47 +0200

    rewrite specific entities test

---
+++ src/ACadSharp.Tests/IO/RewriteSamplesTests.cs
@@ -0,0 +1,41 @@
+﻿using ACadSharp.IO;
+using ACadSharp.IO.DWG;
+using ACadSharp.Tests.TestModels;
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ACadSharp.Tests.IO
+{
+	// Test to read and write local dwg files and compare the result with the original file.
+	public class RewriteSamplesTests : IOTestsBase
+	{
+		public static TheoryData<FileModel> DwgFiles { get; } = new();
+
+		static RewriteSamplesTests()
+		{
+			loadLocalSamples("rewrite", "in.dwg", DwgFiles);
+		}
+
+		public RewriteSamplesTests(ITestOutputHelper output) : base(output)
+		{
+		}
+
+		[Theory]
+		[MemberData(nameof(DwgFiles))]
+		public void ReadUserDwg(FileModel test)
+		{
+			if (string.IsNullOrEmpty(test.Path))
+				return;
+
+			DwgWriterConfiguration writerConfiguration = new DwgWriterConfiguration
+			{
+				WriteXRecords = true,
+			};
+
+			CadDocument doc = DwgReader.Read(test.Path, this._dwgConfiguration, this.onNotification);
+
+			DwgWriter.Write(Path.Combine(test.Folder, $"{test.NoExtensionName}.out.dwg"), doc, writerConfiguration, this.onNotification);
+		}
+	}
+}
