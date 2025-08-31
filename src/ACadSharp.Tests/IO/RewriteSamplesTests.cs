abb70c875525c883be160adc041fd8896eb3e2e9
DomCR <domenech93@gmail.com>
2025-08-31 09:32:50 +0200

    tests

--- src/ACadSharp.Tests/IO/RewriteSamplesTests.cs
+++ src/ACadSharp.Tests/IO/RewriteSamplesTests.cs
@@ -1,7 +1,9 @@
-﻿using ACadSharp.IO;
+﻿using ACadSharp.Entities;
+using ACadSharp.IO;
 using ACadSharp.IO.DWG;
 using ACadSharp.Tests.TestModels;
 using System.IO;
+using System.Linq;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -36,8 +38,6 @@ namespace ACadSharp.Tests.IO
 			CadDocument doc = DwgReader.Read(test.Path, this._dwgConfiguration, this.onNotification);
 
 			DwgWriter.Write(Path.Combine(test.Folder, $"{test.NoExtensionName}.out.dwg"), doc, writerConfiguration, this.onNotification);
-
-			//Mesh fails when written, all the other entities are discarded
 		}
 	}
 }
