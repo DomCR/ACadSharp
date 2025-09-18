0f9154ea6606a457a93e9b7c78acbaad86c0b9d1
DomCR <domenech93@gmail.com>
2025-09-18 11:20:40 +0200

    fix for unassigned handles

--- src/ACadSharp/IO/DWG/DwgDocumentBuilder.cs
+++ src/ACadSharp/IO/DWG/DwgDocumentBuilder.cs
@@ -30,6 +30,8 @@ namespace ACadSharp.IO.DWG
 
 		public override void BuildDocument()
 		{
+			this.createMissingHandles();
+
 			//Set the names for the block records before add them to the table
 			foreach (var item in this.BlockRecordTemplates)
 			{
