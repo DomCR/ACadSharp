418454a1ccdcf06f033f85d1ccbb49a45e26f7da
DomCR <domenech93@gmail.com>
2025-09-20 10:02:21 +0200

    invalid characters

---
+++ src/ACadSharp/Extensions/INamedCadObjectExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace ACadSharp.Extensions
+{
+	public static class INamedCadObjectExtensions
+	{
+		public static readonly char[] InvalidCharacters = { '\\', '/', ':', '*', '?', '"', '<', '>', '|', ';', ',', '=', '`' };
+
+		/// <summary>
+		/// Check if the name of the object is valid for dxf format.
+		/// </summary>
+		/// <param name="namedCadObject"></param>
+		/// <returns></returns>
+		public static bool IsValidDxfName(this INamedCadObject namedCadObject)
+		{
+			if (string.IsNullOrEmpty(namedCadObject.Name))
+			{
+				return false;
+			}
+
+			return namedCadObject.Name.IndexOfAny(InvalidCharacters) == -1;
+		}
+	}
+}
