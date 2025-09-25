5ef87ea1b7cab192e917ced3113e7b92611ab453
DomCR <domenech93@gmail.com>
2025-09-25 09:43:06 +0200

    added length limit

--- src/ACadSharp/Extensions/INamedCadObjectExtensions.cs
+++ src/ACadSharp/Extensions/INamedCadObjectExtensions.cs
@@ -8,10 +8,20 @@
 		/// Check if the name of the object is valid for dxf format.
 		/// </summary>
 		/// <param name="namedCadObject"></param>
+		/// <param name="version"></param>
 		/// <returns></returns>
-		public static bool IsValidDxfName(this INamedCadObject namedCadObject)
+		public static bool IsValidDxfName(this INamedCadObject namedCadObject, ACadVersion version = ACadVersion.AC1032)
 		{
 			if (string.IsNullOrEmpty(namedCadObject.Name))
+			{
+				return false;
+			}
+
+			if (version <= ACadVersion.AC1015 && namedCadObject.Name.Length > 31)
+			{
+				return false;
+			}
+			else if (namedCadObject.Name.Length > 255)
 			{
 				return false;
 			}
