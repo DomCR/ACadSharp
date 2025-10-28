3a21ad65f00a02ae8c4287c3e8969604dd6ae321
DomCR <domenech93@gmail.com>
2025-10-28 09:26:21 +0100

    cleanup

--- src/ACadSharp/Tables/BlockRecord.cs
+++ src/ACadSharp/Tables/BlockRecord.cs
@@ -40,7 +40,7 @@ namespace ACadSharp.Tables
 				Layout layout = new Layout();
 				layout.Name = Layout.ModelLayoutName;
 				layout.AssociatedBlock = record;
-				layout.Flags = ACadSharp.Objects.PlotFlags.UseStandardScale | ACadSharp.Objects.PlotFlags.PlotPlotStyles | ACadSharp.Objects.PlotFlags.PrintLineweights | ACadSharp.Objects.PlotFlags.DrawViewportsFirst | ACadSharp.Objects.PlotFlags.ModelType | ACadSharp.Objects.PlotFlags.UpdatePaper | ACadSharp.Objects.PlotFlags.Initializing;
+				layout.Flags = PlotFlags.UseStandardScale | PlotFlags.PlotPlotStyles | PlotFlags.PrintLineweights | PlotFlags.DrawViewportsFirst | PlotFlags.ModelType | PlotFlags.UpdatePaper | PlotFlags.Initializing;
 				layout.LayoutFlags = LayoutFlags.PaperSpaceLinetypeScaling;
 				layout.MaxLimits = new XY(12, 9);
 				layout.PlotType = PlotType.LastScreenDisplay;
