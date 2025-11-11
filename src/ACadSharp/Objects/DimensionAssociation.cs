ed3aa25794749408cbf26dfc6096a6be5d2abc14
DomCR <domenech93@gmail.com>
2025-11-11 17:15:14 +0100

    DimensionAssociation

---
+++ src/ACadSharp/Objects/DimensionAssociation.cs
@@ -0,0 +1,25 @@
+﻿using ACadSharp.Attributes;
+
+namespace ACadSharp.Objects
+{
+	/// <summary>
+	/// Represents a <see cref="DimensionAssociation"/> object.
+	/// </summary>
+	/// <remarks>
+	/// Dxf class name <see cref="DxfSubclassMarker.DimensionAssociation"/>
+	/// </remarks>
+	[DxfSubClass(DxfSubclassMarker.DimensionAssociation)]
+	public abstract class DimensionAssociation : NonGraphicalObject
+	{
+		/// <inheritdoc/>
+		public override ObjectType ObjectType => ObjectType.UNLISTED;
+
+		/// <inheritdoc/>
+		public override string SubclassMarker => DxfSubclassMarker.DimensionAssociation;
+
+		/// <inheritdoc/>
+		public DimensionAssociation() : base()
+		{
+		}
+	}
+}
