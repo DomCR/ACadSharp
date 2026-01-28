34a38862ad065bb48a7d2c1838affd7b7a9376c5
DomCR <domenech93@gmail.com>
2026-01-28 06:25:35 +0100

    unused var

--- src/ACadSharp/IO/DXF/DxfDocumentBuilder.cs
+++ src/ACadSharp/IO/DXF/DxfDocumentBuilder.cs
@@ -22,8 +22,6 @@ namespace ACadSharp.IO.DXF
 
 		public List<CadTemplate> OrphanTemplates { get; set; } = new();
 
-		public ICadObjectTemplate LastTemplate { get { return this.cadObjectsTemplates.Values.LastOrDefault(); } }
-
 		public DxfDocumentBuilder(ACadVersion version, CadDocument document, DxfReaderConfiguration configuration) : base(version, document)
 		{
 			this.Configuration = configuration;
