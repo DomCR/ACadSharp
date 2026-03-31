466d4ffda6d1af0fbefae82be58d9deacfa8c293
DomCR <domenech93@gmail.com>
2026-03-31 17:25:48 +0200

    Refactor DWG reader; add AcmPartRef and ProxyEntity version

---
+++ src/ACadSharp/Entities/BOM/AcmPartRef.cs
@@ -0,0 +1,21 @@
+﻿using CSMath;
+
+namespace ACadSharp.Entities.BOM;
+
+public class AcmPartRef : Entity
+{
+	public override ObjectType ObjectType { get { return ObjectType.UNLISTED; } }
+
+	public XYZ Position { get; internal set; }
+	public double Radius { get; internal set; }
+
+	public override void ApplyTransform(Transform transform)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public override BoundingBox GetBoundingBox()
+	{
+		throw new System.NotImplementedException();
+	}
+}
