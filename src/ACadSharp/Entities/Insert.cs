2078a04dc2dc2bea139464a14574be3b1bebd68e
DomCR <domenech93@gmail.com>
2026-04-06 12:03:13 +0200

    apply generic transform

--- src/ACadSharp/Entities/Insert.cs
+++ src/ACadSharp/Entities/Insert.cs
@@ -360,16 +360,10 @@ public class Insert : Entity
 	{
 		BoundingBox box = this.Block.GetBoundingBox();
 
-		var scale = new XYZ(this.XScale, this.YScale, this.ZScale);
-		var min = box.Min * scale + this.InsertPoint;
-		var max = box.Max * scale + this.InsertPoint;
+		var t = this.GetTransform();
 
-		if (this.Rotation != 0)
-		{
-			var t = Transform.CreateRotation(this.Normal, this.Rotation);
-			min = t.ApplyRotation(min);
-			max = t.ApplyRotation(max);
-		}
+		var min = t.ApplyTransform(box.Min);
+		var max = t.ApplyTransform(box.Max);
 
 		return new BoundingBox(min, max);
 	}
