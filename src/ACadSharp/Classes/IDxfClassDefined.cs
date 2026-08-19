0e947316afb8bd12a3d538a075e518b324da7273
DomCR <domenech93@gmail.com>
2026-08-19 19:27:57 +0200

    docs

--- src/ACadSharp/Classes/IDxfClassDefined.cs
+++ src/ACadSharp/Classes/IDxfClassDefined.cs
@@ -1,6 +1,13 @@
 ﻿namespace ACadSharp.Classes;
 
+/// <summary>
+/// Defines a contract for classes that can provide a <see cref="DxfClass"/> instance.
+/// </summary>
 public interface IDxfClassDefined
 {
+	/// <summary>
+	/// Gets the <see cref="DxfClass"/> instance associated with this class.
+	/// </summary>
+	/// <returns>The <see cref="DxfClass"/> instance associated with this class.</returns>
 	public DxfClass GetDxfClass();
 }
