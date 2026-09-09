d69d24f128547359f1c33c501c59900407ec817e
DomCR <domenech93@gmail.com>
2026-09-09 10:28:04 +0200

    tests fix

--- src/ACadSharp.Tests/Entities/HatchTests.cs
+++ src/ACadSharp.Tests/Entities/HatchTests.cs
@@ -148,8 +148,8 @@ public class HatchTests : CommonEntityTests<Hatch>
 			.ToList();
 
 		Assert.Single(lines);
-		AssertUtils.AreEqual(new XYZ(-1, 0, 0), lines[0].StartPoint);
-		AssertUtils.AreEqual(new XYZ(1, 0, 0), lines[0].EndPoint);
+		AssertUtils.AreEqual(new XYZ(-1, 0, 0), lines[0].StartPoint, 3);
+		AssertUtils.AreEqual(new XYZ(1, 0, 0), lines[0].EndPoint, 3);
 	}
 
 	[Fact]
