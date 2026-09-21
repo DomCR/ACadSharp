7a50ef5509d642f2f66822098df07bb6f8bc808c
DomCR <domenech93@gmail.com>
2026-09-21 11:54:33 +0200

    Set default Hatch style and remove obsolete comments
    Set Hatch.Style default to HatchStyleType.Normal for clarity.
    Removed unused MPolygon-related comments to clean up code.

--- src/ACadSharp/Entities/Hatch.cs
+++ src/ACadSharp/Entities/Hatch.cs
@@ -128,12 +128,11 @@ public partial class Hatch : Entity, IOrientable
 	[DxfCollectionCodeValue(10, 20)]
 	public List<XY> SeedPoints { get; set; } = new List<XY>();
 
-	//63	For MPolygon, pattern fill color as the ACI
 	/// <summary>
 	/// Hatch style.
 	/// </summary>
 	[DxfCodeValue(75)]
-	public HatchStyleType Style { get; set; }
+	public HatchStyleType Style { get; set; } = HatchStyleType.Normal;
 
 	/// <inheritdoc/>
 	public override string SubclassMarker => DxfSubclassMarker.Hatch;
@@ -142,14 +141,6 @@ public partial class Hatch : Entity, IOrientable
 
 	private double _patternScale;
 
-	//73	For MPolygon, boundary annotation flag:
-	//0 = boundary is not an annotated boundary
-	//1 = boundary is an annotated boundary
-	//78	Number of pattern definition lines
-	//varies
-	//Pattern line data.Repeats number of times specified by code 78. See Pattern Data
-	//11	For MPolygon, offset vector
-	//99	For MPolygon, number of degenerate boundary paths(loops), where a degenerate boundary path is a border that is ignored by the hatch
 	/// <inheritdoc/>
 	public Hatch() : base() { }
 
